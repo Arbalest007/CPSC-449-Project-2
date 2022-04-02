86f67f9864174dbf8e45b603731c914a23add91d
Patrick Lin <51501192+Arbalest007@users.noreply.github.com>
2022-04-02 15:25:16 -0700

    Tentative Code for Adding/Removing Guesses
    -Added POST methods to add or remove new guess words into the Words DB
    -Added additional Try/Except for Words DB initialization so that the DB doesn't insert duplicate values if the script is called again
    -Added Status Codes
    -Added wordList Base Model to define how we pass in a list of words to the Add/Remove POST methods

--- WordsDB.py
+++ WordsDB.py
@@ -8,23 +8,26 @@ cur.execute("CREATE TABLE IF NOT EXISTS t (Words TEXT, UNIQUE(Words))")
 
 #Filter out the strings for Words Microservice from defined source as per Project Reqs
 #and import into sqlite3 DB
-with fileinput.input(files=('/usr/share/dict/words')) as f:
-    for line in f:
-        if line.islower():
-            if line.isascii():
-                if not('\'' in line):
-                    #Need to remove \n from end of string
-                    cleaned_line = line.replace("\n", "")
-                    
-                    if len(line) == 6:
-                        #Debugging
-                        #print(line)
+try:
+    with fileinput.input(files=('/usr/share/dict/words')) as f:
+        for line in f:
+            if line.islower():
+                if line.isascii():
+                    if not('\'' in line):
+                        #Need to remove \n from end of string
+                        cleaned_line = line.replace("\n", "")
 
-                        #cur.execute("INSERT INTO t (Words) VALUES (?)", (line,))
-                        cur.execute("INSERT INTO t VALUES(?)", (cleaned_line,))
-                        con.commit()
-
-                        #Debugging
-                        #print("Word has been inserted!")
+                        if len(line) == 6:
+                            #Debugging
+                            #print(line)
+                          
+                            #cur.execute("INSERT INTO t (Words) VALUES (?)", (line,))
+                            cur.execute("INSERT INTO t VALUES(?)", (cleaned_line,))
+                            con.commit()
+                       
+                            #Debugging
+                            #print("Word has been inserted!")
+except:
+    print("DB is already populated with pre-defined words!")
 
 con.close()
